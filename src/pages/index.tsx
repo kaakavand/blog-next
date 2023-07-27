56eea4a512a4a04b3368df46688a48adf02fe290
Amirhosein Kakavand <94612720+kaakavand@users.noreply.github.com>
2023-07-27 13:32:19 +0330

    fix categpry sidbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <div className='grid gap-4 md:grid-cols-12  min-h-screen md:grid-rows-[50px_minmax(300px,_1fr)]'>
-        <div className='rounded-md border bg-slate-800 border-slate-900 hidden md:block md:row-span-2 md:col-span-3'>
+    <div className="container mx-auto px-4">
+      <div className='grid gap-4 md:grid-cols-12  min-h-screen md:grid-rows-[48px_minmax(300px,_1fr)]'>
+        <div className='rounded-md h-fit border bg-slate-800 border-slate-900 hidden md:block md:row-span-2 md:col-span-3'>
           <div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between h-12 px-4 text-gray-400  cursor-pointer">
               <span>دسته بندی مقالات</span>
-              <ChevronDownIcon className="w-6 h-6 stroke-orange-300" />
+              <ChevronDownIcon className="w-6 h-6 strok-gray-400" />
             </div>
-            <div>
+            <div className="px-4">
+              <ul className="mb-4">
+                <li
+                  className="flex items-center text-gray-100 py-2 mb-1 rounded-md cursor-pointer ease-linear duration-200">
+                  <span>گوشی موبایل</span>
+                </li>
+                <li
+                  className="flex items-center text-gray-100 py-2 mb-1 rounded-md cursor-pointer ease-linear duration-200">
+                  <span>گوشی موبایل</span>
+                </li>
+                <li
+                  className="flex items-center text-gray-100 py-2 mb-1 rounded-md cursor-pointer ease-linear duration-200">
+                  <span>گوشی موبایل</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className='hidden md:col-span-9 md:flex items-center'>
+        <div className=' md:col-span-9 md:flex items-center'>
           <div className="border flex items-center rounded-md px-4 h-full w-full bg-slate-800 border-slate-900">
             <div className="flex items-center text-gray-400">
               <AdjustmentsHorizontalIcon className="w-5 h-5 mt-0.5" />
@@ -39,7 +53,7 @@ export default function Home() {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(el =>
               <div className="col-span-6 md:col-span-6 lg:col-span-4 xl:col-span-3 bg-sky-900 rounded-md h-fit p-3 relative mt-8">
                 <figure className="-mt-8 rounded-md overflow-hidden mb-3">
-                 <Image src={imagedf} alt="d"/>
+                  <Image src={imagedf} alt="d" />
                 </figure>
                 <div>
                   <h3 className="text-gray-100 text-2xl font-black mb-3">
